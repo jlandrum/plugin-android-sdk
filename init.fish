32246dbdf3902d6437ab2140568915e208a0b36e
James Landrum <me@jameslandrum.com>
2020-03-13 22:37:39 -0400

    Update init.fish

--- init.fish
+++ init.fish
@@ -1,11 +1,7 @@
 set paths $ANDROID_HOME/{tool?,platform-tool?}
-if test -z "$paths"
-  set paths $ANDROID_SDK_ROOT/{tool?,platform-tool?}
-else if test -z "$paths"
-  set paths $HOME/Android/Sdk/{tool?,platform-tool?}
-end
-
-echo "Settings paths: $paths"
+test -n "$paths"; or set paths $ANDROID_SDK_ROOT/{tool?,platform-tool?}
+test -n "$paths"; or set paths $HOME/Android/Sdk/{tool?,platform-tool?}
+test -n "$paths"; or set paths $HOME/Library/Android/sdk/{tool?,platform-tool?}
 
 if test (count $paths) -gt 0
   set -l IFS /
@@ -15,5 +11,4 @@ if test (count $paths) -gt 0
 
   set -gx PATH $PATH $paths
   set -gx ANDROID_HOME $root
-  echo "Paths set successfully"
 end
